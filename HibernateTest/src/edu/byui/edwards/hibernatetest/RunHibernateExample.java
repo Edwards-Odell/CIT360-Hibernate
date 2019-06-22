89fb504d18079899d78ebb34a17f05e8e1c0ce20
Odell Edwards <odelledwards1@gmail.com>
2019-06-21 22:34:43 -0400

    Initial commit

---
+++ HibernateTest/src/edu/byui/edwards/hibernatetest/RunHibernateExample.java
@@ -0,0 +1,26 @@
+package edu.byui.edwards.hibernatetest;
+
+/* This program is a part of a simple hibernate example used for CIT-360
+It is written by Troy Tuckett.  BYUI.EDU
+*/
+// package edu.byui.tuckett.hibernatetest;
+
+import java.util.*;
+
+public class RunHibernateExample {
+
+ public static void main(String[] args) {
+
+     TestDAO t = TestDAO.getInstance();
+
+     List<Customer> c = t.getCustomers();
+     for (Customer i : c) {
+         System.out.println(i);
+     }
+
+     System.out.println(t.getCustomer(1));
+ }
+}
+/*
+Trying to figure out why this thing wont commit.
+*/
